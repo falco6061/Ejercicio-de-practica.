becb7ba2cd7324d912691d6ff11c90a1ba0b7c2c
falco6061 <treynoso@escuelasproa.edu.ar>
2026-05-07 14:30:42 -0300

    Add files via upload

---
+++ ESTRUCTURA/hola.py
@@ -0,0 +1,11 @@
+class Animal:
+    def hablar(self):
+        print("Este animal emite un sonido")
+
+class gato(Animal):
+    def maullar(self):
+        print("¡miau! ¡miau!")
+
+mi_gato= Gato()
+mi_gato.hablar()
+mi_gato.maullar()
